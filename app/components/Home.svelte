ef039559578c2b7b3d31d97452a5748ef284defe
gbarska <gbarska@yahoo.com>
2023-02-23 10:28:39 -0300

    First Commit

---
+++ app/components/Home.svelte
@@ -0,0 +1,24 @@
+<page>
+    <actionBar title="Home" />
+    <gridLayout>
+        <MyText text="hello" />
+    </gridLayout>
+</page>
+
+<script lang="ts">
+    import { MyText } from 'gb7sveltelib';
+
+    let message: string = "Blank Svelte Native App"
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
